a9025beb6dae224e8b068879f13ec816bcfb44f6
Tapatronic <151563667+Tapatronic@users.noreply.github.com>
2023-11-28 21:44:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-window.onload = function() {
-    var horaConexion = new Date();
 
-    // Muestra la hora de conexión en el footer
-    document.getElementById('horaConexion').textContent = 'Hora de conexión: ' + horaConexion.toLocaleTimeString();
-
-    // Actualiza el tiempo de conexión cada segundo
-    setInterval(function() {
-        var ahora = new Date();
-        var tiempoConexion = Math.floor((ahora - horaConexion) / 1000);
-        document.getElementById('tiempoConexion').textContent = 'Tiempo de conexión: ' + tiempoConexion + ' segundos';
-    }, 1000);
