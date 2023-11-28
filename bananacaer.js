4521f753c3e5b9d8fed4f4790a02dd08ce694025
Tapatronic <151563667+Tapatronic@users.noreply.github.com>
2023-11-28 22:11:10 +0100

    Update bananacaer.js

--- bananacaer.js
+++ bananacaer.js
@@ -1,3 +1,16 @@
+window.onload = function() {
+    var horaConexion = new Date();
+
+    // Muestra la hora de conexión en el footer
+    document.getElementById('horaConexion').textContent = 'Hora de conexión: ' + horaConexion.toLocaleTimeString();
+
+    // Actualiza el tiempo de conexión cada segundo
+    setInterval(function() {
+        var ahora = new Date();
+        var tiempoConexion = Math.floor((ahora - horaConexion) / 1000);
+        document.getElementById('tiempoConexion').textContent = 'Tiempo de conexión: ' + tiempoConexion + ' segundos';
+    }, 1000);
+});
 document.body.addEventListener('click', function(event) {
     var bananaImg = document.createElement('img');
     bananaImg.src = 'banana.png';
@@ -15,17 +28,4 @@ document.body.addEventListener('click', function(event) {
     bananaImg.addEventListener('transitionend', function() {
         document.body.removeChild(bananaImg);
     });
-});
-window.onload = function() {
-    var horaConexion = new Date();
-
-    // Muestra la hora de conexión en el footer
-    document.getElementById('horaConexion').textContent = 'Hora de conexión: ' + horaConexion.toLocaleTimeString();
-
-    // Actualiza el tiempo de conexión cada segundo
-    setInterval(function() {
-        var ahora = new Date();
-        var tiempoConexion = Math.floor((ahora - horaConexion) / 1000);
-        document.getElementById('tiempoConexion').textContent = 'Tiempo de conexión: ' + tiempoConexion + ' segundos';
-    }, 1000);
 });
