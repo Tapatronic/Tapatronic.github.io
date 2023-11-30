9ac3c41a2aadddfb102722dc29630fb3b3cc0e13
Tapatronic <151563667+Tapatronic@users.noreply.github.com>
2023-11-30 15:55:52 +0100

    Update bananacaer.js

--- bananacaer.js
+++ bananacaer.js
@@ -1,15 +1,10 @@
-document.body.addEventListener('click', function(event) {
-    var bananaImg = document.createElement('img');
-    bananaImg.src = 'banana.png';
-    bananaImg.style.left = event.clientX + 'px';
-    bananaImg.style.top = event.clientY + 'px';
-    bananaImg.classList.add('banana');
-    document.body.appendChild(bananaImg);
-    bananaImg.style.transition = 'transform 10s';
-    setTimeout(function() {
-        bananaImg.style.transform = 'rotate(360deg) translateY(' + window.innerHeight + 'px)';
-    }, 0);
-    bananaImg.addEventListener('transitionend', function() {
-        document.body.removeChild(bananaImg);
-    });
-});
+        document.body.addEventListener('click', function(event) {
+            var banana = document.getElementById('banana');
+            banana.style.left = event.clientX + 'px';
+            banana.style.top = event.clientY + 'px';
+            setTimeout(function() {
+                banana.style.transform = 'translateY(' + window.innerHeight + 'px)';
+                banana.classList.remove('banana1');
+                banana.classList.add('banana2');
+            }, 0);
+        });
