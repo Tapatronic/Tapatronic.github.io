6f117799f42698368b93768d35c07984f0871808
Tapatronic <151563667+Tapatronic@users.noreply.github.com>
2024-01-17 19:27:24 +0100

    traductor autologger

--- traductorgoogle.js
+++ traductorgoogle.js
@@ -1,11 +1,6 @@
-$('.translation-links').click(function() {
-  var lang = $(this).data('lang');
-  var $frame = $('.goog-te-menu-frame:first');
-  if (!$frame.size()) {
-    alert("Error: Could not find Google translate frame.");
-    return false;
-  }
-  $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
-  return false;
-});
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
+        }
+
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
